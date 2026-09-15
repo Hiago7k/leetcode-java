e0a22237834e95be3a7f0b8048bb92fa504f2ab7
Hiago 7k <hiago7k@gmail.com>
2026-09-15 17:05:59 -0300

    primeira parte pegando o menor e o maior valor das stocks

--- src/BestTimeToBytAndSellStock.java
+++ src/BestTimeToBytAndSellStock.java
@@ -1,7 +1,25 @@
+import java.util.Arrays;
+
 public class BestTimeToBytAndSellStock {
     static void main() {
-        int num[] = {7, 1, 5, 3, 6, 4};
+        int nums[] = {7, 1, 5, 3, 6, 4};
+        int lower = nums[0];
+        int higher = nums[0];
+        int sell = 0;
 
-        System.out.println(num[0]);
+        // primeira parte saber o menor valor
+        for(int i = 0; i <= nums.length -1; i++){
+            if(nums[i] <= lower){
+                lower=+ nums[i];
+            }
+            for(int j = i + 1; j <= nums.length -1; j++){
+                if(nums[j] >= higher){
+                    higher=+ nums[j];
+                }
+            }
+            sell = higher - lower -1;
+            // outra validacao se os precos so cairem nao faz nada
+        }
+        System.out.println(sell);
     }
 }
