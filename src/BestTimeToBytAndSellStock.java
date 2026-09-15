a6fd17957dbe3f42227ff0e6eec41f3aa94a299a
Hiago 7k <hiago7k@gmail.com>
2026-09-15 18:47:55 -0300

    bug logica

--- src/BestTimeToBytAndSellStock.java
+++ src/BestTimeToBytAndSellStock.java
@@ -2,24 +2,22 @@ import java.util.Arrays;
 
 public class BestTimeToBytAndSellStock {
     static void main() {
-        int nums[] = {7, 1, 5, 3, 6, 4};
-        int lower = nums[0];
-        int higher = nums[0];
-        int sell = 0;
+       int nums[] = {7, 2, 5, 3, 6, 4};
+       int sell = 0;
+       int higher = 1;
+       int lower = 1;
 
-        // primeira parte saber o menor valor
         for(int i = 0; i <= nums.length -1; i++){
             if(nums[i] <= lower){
-                lower=+ nums[i];
+                lower = lower = nums[i];
             }
             for(int j = i + 1; j <= nums.length -1; j++){
                 if(nums[j] >= higher){
-                    higher=+ nums[j];
+                   higher = higher = nums[j];
                 }
             }
-            sell = higher - lower -1;
-            // outra validacao se os precos so cairem nao faz nada
         }
+        sell = sell = higher - lower;
         System.out.println(sell);
     }
 }
